01545a99d5b93bb7c46986df815f2c45bd947538
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-08-13 15:36:23 +0000

    updated script

--- FruitDetector/app/server.py
+++ FruitDetector/app/server.py
@@ -25,9 +25,9 @@ async def download_file(url, dest):
             with open(dest, 'wb') as f: f.write(data)
 
 async def setup_learner():
-    await download_file(export_file_url, path/export_file_name)
+#    await download_file(export_file_url, path/export_file_name)
     try:
-        learn = load_learner(path, export_file_name)
+        learn = load_learner(path/models, export_file_name)
         return learn
     except RuntimeError as e:
         if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
